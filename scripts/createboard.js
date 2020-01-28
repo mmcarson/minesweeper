7c5ded18bbabae79e27dfd9259854e39a8767aa1
Marissa Carson <mmcarson@coe.edu>
2020-01-28 10:42:45 -0600

    made a class
    and filled all colors and numbers

--- scripts/createboard.js
+++ scripts/createboard.js
@@ -1,18 +1,45 @@
 const RIGHT = 2;
 const LEFT = 0;
 
-function createBoard() {
-    console.log("creating board...");
-    const boardElement = document.getElementById("board");
-    for (let i = 0; i < 900; i++) {
-        let squareElement = document.createElement("div");
-        squareElement.classList.add("square");
-        squareElement.addEventListener("mousedown", event => {
-            if (event.button == RIGHT) {
-                squareElement.classList.add("flag");
-            }
-        });
-        boardElement.appendChild(squareElement);
+const COLORS = ["blue", "green", "red", "navy", "maroon", "teal", "black", "gray"];
+
+function addParagraph(text, parent) {
+    p = document.createElement("p");
+    p.innerText = text;
+    p.style.color = (COLORS[text - 1]);
+    parent.appendChild(p);
+}
+
+function addMines(w, h, n) {
+
+}
+
+class Board {
+    constructor(width, height, mines) {
+        this.width = width
+        this.height = height
+        this.mines = mines
+
+        const boardElement = document.getElementById("board");
+        boardElement.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
+        for (let i = 0; i < width * height; i++) {
+            let squareElement = document.createElement("div");
+            squareElement.classList.add("square");
+            addParagraph(Math.ceil(Math.random() * 8), squareElement);
+            squareElement.addEventListener("mousedown", event => {
+                if (event.button == RIGHT) {
+                    squareElement.classList.add("flag");
+                } else if (event.button == LEFT) {
+                    squareElement.classList.add("opened");
+                }
+            });
+            boardElement.appendChild(squareElement);
+        }
     }
+}
+
+function createBoard(width, height, mines) {
+    console.log("creating board...");
+    const myBoard = new Board(width, height, mines);
     console.log("board creation completed.");
 }
