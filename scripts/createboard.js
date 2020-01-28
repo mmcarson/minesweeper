54fe787c0bf3862bf1d8aea38ac1067a8cec83f3
Marissa Carson <mmcarson@coe.edu>
2020-01-28 11:15:44 -0600

    working on neighbors

--- scripts/createboard.js
+++ scripts/createboard.js
@@ -1,17 +1,13 @@
-const RIGHT = 2;
-const LEFT = 0;
+const RIGHT = 2
+const LEFT = 0
 
-const COLORS = ["blue", "green", "red", "navy", "maroon", "teal", "black", "gray"];
+const COLORS = ["blue", "green", "red", "navy", "maroon", "teal", "black", "gray"]
 
 function addParagraph(text, parent) {
-    p = document.createElement("p");
-    p.innerText = text;
-    p.style.color = (COLORS[text - 1]);
-    parent.appendChild(p);
-}
-
-function addMines(w, h, n) {
-
+    p = document.createElement("p")
+    p.innerText = text
+    p.style.color = (COLORS[text - 1])
+    parent.appendChild(p)
 }
 
 class Board {
@@ -19,27 +15,66 @@ class Board {
         this.width = width
         this.height = height
         this.mines = mines
+        this.addMines()
 
-        const boardElement = document.getElementById("board");
-        boardElement.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
+        const boardElement = document.getElementById("board")
+        boardElement.style.gridTemplateColumns = `repeat(${width}, 1fr)`
         for (let i = 0; i < width * height; i++) {
-            let squareElement = document.createElement("div");
-            squareElement.classList.add("square");
-            addParagraph(Math.ceil(Math.random() * 8), squareElement);
+            let squareElement = document.createElement("div")
+            squareElement.classList.add("square")
+            addParagraph(Math.ceil(Math.random() * 8), squareElement)
             squareElement.addEventListener("mousedown", event => {
                 if (event.button == RIGHT) {
-                    squareElement.classList.add("flag");
+                    squareElement.classList.add("flag")
                 } else if (event.button == LEFT) {
-                    squareElement.classList.add("opened");
+                    squareElement.classList.add("opened")
                 }
-            });
-            boardElement.appendChild(squareElement);
+            })
+            boardElement.appendChild(squareElement)
         }
+
+        this.boardElement = boardElement
+    }
+
+    addMines() {
+        this.mineSet = new Set()
+        while (this.mineSet.size < this.mines) {
+            this.mineSet.add(Math.floor(Math.random() * this.width * this.height))
+        }
+        console.log(this.mineSet)
+    }
+
+    findIndex(x, y) {
+        return y * this.width + x
+    }
+
+    findNeighbors(index) {
+        const neighbors = [],
+            y = Math.floor(index / this.width),
+            x = index % this.width
+
+        // for (let i=-1; i<=1; i++) {
+        //     for (let j=-1; j<=1; j++) {
+        //         square = index + 
+        //     }
+        // }
+        // // left
+        // neighbors.push(index - 1)
+        // // right
+        // neighbors.push(index + 1)
+        // // above
+        // neighbors.push(index - this.width)
+        // // below
+        // neighbors.push(index + this.width)
+        // // upper left
+        // neighbors.push(index - this.width - 1)
+        // // upper right
+        // neighbors.push(index - this.width + 1)
     }
 }
 
 function createBoard(width, height, mines) {
-    console.log("creating board...");
-    const myBoard = new Board(width, height, mines);
-    console.log("board creation completed.");
+    console.log("creating board...")
+    const myBoard = new Board(width, height, mines)
+    console.log("board creation completed.")
 }
